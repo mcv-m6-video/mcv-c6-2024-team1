14f90d8fea6d9de3ebb9d94886be1668afdb1063
adrisubi <adria.subi@gmail.com>
2024-03-04 17:33:23 +0100

    added tqdm and formatted code

--- Week2/visualize_tracking.py
+++ Week2/visualize_tracking.py
@@ -1,8 +1,8 @@
 import cv2
 import json
 import colorsys
-import os
-import numpy as np
+from tqdm import tqdm
+import random
 
 
 VIDEO_PATH = '../Data/AICity_data/train/S03/c010/vdo.avi'
@@ -33,82 +33,83 @@ def generate_rainbow_cv2_colors(num_colors):
 
 
 def visualize_tracking(video_path, annotations_path, video_out_path, save_video=True, visualize=True):
-    # Open video file
+    # Open video file with tqdm
     cap = cv2.VideoCapture(video_path)
 
-    # Check if video file opened successfully
-    if not cap.isOpened():
-        print("Error opening video file")
-        raise
+    with tqdm(total=int(cap.get(cv2.CAP_PROP_FRAME_COUNT)), desc='Loading visualization') as pbar:
+        # Check if video file opened successfully
+        if not cap.isOpened():
+            print("Error opening video file")
+            raise
 
-    with open(annotations_path, 'r') as f:
-        bbxs = json.load(f)
-
-    if save_video:
-        output_video_filename = video_out_path
-        codec = cv2.VideoWriter_fourcc(*'XVID')  # Codec for AVI format
-        fps = cap.get(cv2.CAP_PROP_FPS)  # Frames per second
-        frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-        out = cv2.VideoWriter(output_video_filename, codec,
-                              fps, (frame_width, frame_height))
-
-    if cap.get(cv2.CAP_PROP_FRAME_COUNT) != len(bbxs):
-        print('Num of frames in video != num of frames in annotations')
-        raise
-
-    max_track = 0
-    for bbx in bbxs:
-        max_track_now = max(bbx['track'].values())
-        max_track = max(max_track_now, max_track)
-
-    i = 0
-    colors_table = generate_rainbow_cv2_colors(max_track + 1)
-
-    # Loop through each frame of the video
-    while cap.isOpened():
-        # Read frame from video
-        ret, frame = cap.read()
-
-        # Check if frame was successfully read
-        if not ret:
-            if i + 1 != len(bbxs):
-                print(f'')
-                print("Error reading frame")
-            break
-
-        img_draw = frame.copy()
-        for k in bbxs[i]['track']:
-            tl = (round(bbxs[i]['xmin'][k]), round(bbxs[i]['ymin'][k]))
-            br = (round(bbxs[i]['xmax'][k]), round(bbxs[i]['ymax'][k]))
-            img_draw = cv2.rectangle(
-                img_draw, (tl), (br), colors_table[bbxs[i]['track'][k]], 2)
-            img_draw = cv2.putText(
-                img_draw, str(bbxs[i]['track'][k]), (tl), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
-            # Draw circles for previous detections
-            if i != 0:
-                prev_bbxs_id = ""
-                for j in bbxs[i-1]['track']:
-                    if bbxs[i-1]['track'][j] == bbxs[i]['track'][k]:
-                        prev_bbxs_id = j
-                if prev_bbxs_id != "":
-                    detection_center = (int((bbxs[i-1]['xmin'][prev_bbxs_id] + bbxs[i-1]['xmax'][prev_bbxs_id]) / 2),
-                                        int((bbxs[i-1]['ymin'][prev_bbxs_id] + bbxs[i-1]['ymax'][prev_bbxs_id]) / 2))
-                    img_draw = cv2.circle(img_draw, detection_center, 5, colors_table[bbxs[i]['track'][k]], -1)
-
-        if visualize:
-            print(f'Frame {i + 1}/{len(bbxs)}')
-            cv2.imshow('Tracking results', cv2.resize(
-                img_draw, (int(img_draw.shape[1]*0.5), int(img_draw.shape[0]*0.5))))
-            k = cv2.waitKey(1)
-            if k == ord('q'):
-                return
+        with open(annotations_path, 'r') as f:
+            bbxs = json.load(f)
 
         if save_video:
-            out.write(img_draw)
+            output_video_filename = video_out_path
+            codec = cv2.VideoWriter_fourcc(*'XVID')  # Codec for AVI format
+            fps = cap.get(cv2.CAP_PROP_FPS)  # Frames per second
+            frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+            frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-        i += 1
+            out = cv2.VideoWriter(output_video_filename, codec,
+                                fps, (frame_width, frame_height))
+
+        if cap.get(cv2.CAP_PROP_FRAME_COUNT) != len(bbxs):
+            print('Num of frames in video != num of frames in annotations')
+            raise
+
+        max_track = 0
+        for bbx in bbxs:
+            max_track_now = max(bbx['track'].values())
+            max_track = max(max_track_now, max_track)
+
+        i = 0
+        colors_table = generate_rainbow_cv2_colors(max_track + 1)
+        random.shuffle(colors_table)
+
+        # Loop through each frame of the video
+        while cap.isOpened():
+            # Read frame from video
+            ret, frame = cap.read()
+
+            # Check if frame was successfully read
+            if not ret:
+                if i + 1 < len(bbxs):
+                    print("Error reading frame")
+                break
+
+            img_draw = frame.copy()
+            for k in bbxs[i]['track']:
+                tl = (round(bbxs[i]['xmin'][k]), round(bbxs[i]['ymin'][k]))
+                br = (round(bbxs[i]['xmax'][k]), round(bbxs[i]['ymax'][k]))
+                img_draw = cv2.rectangle(
+                    img_draw, (tl), (br), colors_table[bbxs[i]['track'][k]], 2)
+                img_draw = cv2.putText(
+                    img_draw, str(bbxs[i]['track'][k]), (tl), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
+                # Draw circles for previous detections
+                if i != 0:
+                    prev_bbxs_id = ""
+                    for j in bbxs[i-1]['track']:
+                        if bbxs[i-1]['track'][j] == bbxs[i]['track'][k]:
+                            prev_bbxs_id = j
+                    if prev_bbxs_id != "":
+                        detection_center = (int((bbxs[i-1]['xmin'][prev_bbxs_id] + bbxs[i-1]['xmax'][prev_bbxs_id]) / 2),
+                                            int((bbxs[i-1]['ymin'][prev_bbxs_id] + bbxs[i-1]['ymax'][prev_bbxs_id]) / 2))
+                        img_draw = cv2.circle(img_draw, detection_center, 5, colors_table[bbxs[i]['track'][k]], -1)
+
+            if visualize:
+                cv2.imshow('Tracking results', cv2.resize(
+                    img_draw, (int(img_draw.shape[1]*0.5), int(img_draw.shape[0]*0.5))))
+                k = cv2.waitKey(1)
+                if k == ord('q'):
+                    visualize = False
+
+            if save_video:
+                out.write(img_draw)
+
+            i += 1
+            pbar.update(1)
 
     if save_video:
         out.release()
