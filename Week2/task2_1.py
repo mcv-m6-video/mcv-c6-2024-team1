5147f69bc95204a3d2b86faf6d3f5be1992b5189
adrisubi <adria.subi@gmail.com>
2024-03-04 17:33:23 +0100

    added tqdm and formatted code

--- Week2/task2_1.py
+++ Week2/task2_1.py
@@ -1,11 +1,17 @@
 import json
+from tqdm import tqdm
 
 from week_utils import *
+from visualize_tracking import visualize_tracking
 
 RESULTS_PATH = './results/'
 FILE_IN = 'bbxs_clean.json'
 FILE_OUT = 'bbxs_clean_tracked.json'
 
+VIDEO_PATH = '../Data/AICity_data/train/S03/c010/vdo.avi'
+VIDEO_OUT_PATH = './results/tracking_vdo.avi'
+SAVE = True
+VISUALIZE = True
 
 def cal_IoU(prev_tl, prev_br, new_tl, new_br):
     # Calculate coordinates of the intersection rectangle
@@ -43,8 +49,7 @@ def track_max_overlap(file_in, file_out, iou_thr=0.4):
     f.close()
 
     # Iterate over each dictionary in the list
-    for i in range(len(bbxs)):
-        print(f'\nFrame {i}')
+    for i in tqdm(range(len(bbxs)), desc='Tracking bounding boxes'):
 
         track_ids = {}
 
@@ -68,39 +73,29 @@ def track_max_overlap(file_in, file_out, iou_thr=0.4):
                     if bbxs[i]['class'][k] == bbxs[i-1]['class'][j]:
                         prev_tl = (bbxs[i-1]['xmin'][j], bbxs[i-1]['ymin'][j])
                         prev_br = (bbxs[i-1]['xmax'][j], bbxs[i-1]['ymax'][j])
-                        # print(f'Prev bbx: {prev_tl}, {prev_br}')
-                        # print(f'New bbx: {new_tl}, {new_br}')
 
                         # Calculate IoU
                         iou = cal_IoU(prev_tl, prev_br, new_tl, new_br)
-                        # print(f"IoU of track {j}: {iou}")
 
                         if iou > max_iou and iou > iou_thr:
                             max_iou = iou
                             max_idx = j
-                            # print(f'max_iou {max_idx}: {max_iou}')
 
-                # TODO(?): comprobar si el track ja s'ha posat a una altra bbx
                 # Check if any bbx passed though the iou thershold
                 if max_iou == -1:
                     # New track id
                     track_ids[k] = max(bbxs[i-1]['track'].values()) + 1
-                    # print(f'New track: {track_ids[k]}')
                 else:
                     # Put track id of bbx with max iou
                     track_ids[k] = bbxs[i-1]['track'][max_idx]
-                    # print(f'Track followed: {track_ids[k]} with iou {max_iou}')
 
         bbxs[i]['track'] = track_ids
-        # print(f'track_ids: {track_ids}')
-        print(f"bbxs[i]['track']: {bbxs[i]['track']}")
-        # print(f"bbxs[i]['track']: {bbxs[i]}")
-        # if i == 3:
-        #    break
 
     save_json(bbxs, file_out)
 
-# TODO: visualization of tracking
 
 if __name__ == "__main__":
     track_max_overlap(RESULTS_PATH + FILE_IN, RESULTS_PATH + FILE_OUT)
+    if SAVE or VISUALIZE:
+        visualize_tracking(VIDEO_PATH, RESULTS_PATH + FILE_OUT,
+                       VIDEO_OUT_PATH, SAVE, VISUALIZE)
