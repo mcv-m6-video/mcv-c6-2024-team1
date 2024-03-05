63d5e8636ff762f979c0d1bb6299a644b4000bd2
Pau Vallespí <53973659+pauvallespi16@users.noreply.github.com>
2024-03-05 22:02:48 +0100

    Fix some errors with imports

--- Week2/task1_4.py
+++ Week2/task1_4.py
@@ -1,37 +1,8 @@
 from argparse import ArgumentParser
 
-import numpy as np
 import torch
 
 from task1_3 import run_finetuning
-
-
-def split_strategy_A(number_of_frames):
-    FRAME_25_PERCENT = int(number_of_frames / 4)
-    train_idxs = np.arange(0, FRAME_25_PERCENT)
-    test_idxs = np.arange(FRAME_25_PERCENT, number_of_frames)
-    return train_idxs, test_idxs
-
-
-def split_strategy_B(fold_idx, number_of_frames):
-    # Assuming 4 folds
-    fold_size = number_of_frames / 4
-    train_idxs = []
-    test_idxs = []
-    for i in range(4):
-        if i == fold_idx:
-            test_idxs.extend(range(i * fold_size, (i + 1) * fold_size))
-        else:
-            train_idxs.extend(range(i * fold_size, (i + 1) * fold_size))
-    return train_idxs, test_idxs
-
-
-def split_strategy_C(number_of_frames):
-    train_size = int(0.25 * number_of_frames)
-    train_idxs = np.random.choice(number_of_frames, train_size, replace=False)
-    test_idxs = np.setdiff1d(np.arange(number_of_frames), train_idxs)
-    return train_idxs, test_idxs
-
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -43,7 +14,7 @@ if __name__ == "__main__":
         help="Strategy to use",
     )
     parser.add_argument(
-        "--fold_index",
+        "--fold-index",
         type=int,
         default=0,
         choices=[0, 1, 2, 3],
