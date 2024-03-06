6cead56bc6902e9bac214cbed2237d31a78c65ae
blastoise-pro <32550964+blastoise-pro@users.noreply.github.com>
2024-03-06 14:33:08 +0100

    multiple conversion

--- Week2/convert_to_trackeval.py
+++ Week2/convert_to_trackeval.py
@@ -2,13 +2,16 @@ import json
 from week_utils import readXMLtoAnnotation
 import xml.etree.ElementTree as ET
 from tqdm import tqdm
+from pathlib import Path
 
 bboxes_2_1 = "./Week2/results/bbxs_clean_tracked.json"
 out_2_1 = "./Week2/results/bbxs_clean_tracked.txt"
-bboxes_2_2 = "./Week2/results/kalman.json"
-out_2_2 = "./Week2/results/kalman.txt"
+
 gt_annot = "./Data/ai_challenge_s03_c010-full_annotation.xml"
 out_gt = "./Week2/results/s03_gt.txt"
+
+kalman_ins = ["./Week2/results/kalman.json", "./Week2/results/kalman_10_3.json", "./Week2/results/kalman_30_3.json", "./Week2/results/kalman_50_3.json", "./Week2/results/kalman25_4.json", "./Week2/results/kalman0.json", ]
+out_folder = "./Week2/results/"
 
 def write_bbxs_to_csv(data, out):
     with open(out, "w") as f:
@@ -34,7 +37,7 @@ def write_kalman_to_csv(data, out):
                 f.write(f'{int(frame) + 1}, {track}, {oframe["x_min"]}, {oframe["y_min"]}, {oframe["x_max"] - oframe["x_min"]}, {oframe["y_max"] - oframe["y_min"]}, 1, -1, -1, -1\n')
 
 
-def XML_to_csv(annots, out, remParked=True):
+def XML_to_csv(annots, out, remParked=False):
     file = ET.parse(annots)
     root = file.getroot()
 
@@ -59,12 +62,15 @@ def XML_to_csv(annots, out, remParked=True):
                     f.write(f"{int(frame) + 1}, {id}, {xtl}, {ytl}, {xbr - xtl}, {ybr - ytl}, 1, -1, -1, -1\n")
 
 
-with open(bboxes_2_1) as f:
-    data = json.load(f)
-write_bbxs_to_csv(data, out_2_1)
+#with open(bboxes_2_1) as f:
+#    data = json.load(f)
+#write_bbxs_to_csv(data, out_2_1)
 
-with open(bboxes_2_2) as f:
-    data = json.load(f)
-write_kalman_to_csv(data, out_2_2)
+for bbxs in kalman_ins:
+    out = Path(out_folder) / Path(bbxs).stem / "data" / "S03aicity-01.txt"
+    out.parent.mkdir(exist_ok=True, parents=True)
+    with open(bbxs) as f:
+        data = json.load(f)
+    write_kalman_to_csv(data, out)
 
-XML_to_csv(gt_annot, out_gt)
+#XML_to_csv(gt_annot, out_gt)
