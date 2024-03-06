ca146f75955a19516c316daf80cb3cc1f948c6fa
marcps8 <marcpsabater@hotmail.com>
2024-03-06 10:44:27 +0100

    changes in task 1.3 and task 1.4

--- Week2/task1_3.py
+++ Week2/task1_3.py
@@ -2,13 +2,14 @@ import os
 
 import torch
 import torchvision
+from sklearn.model_selection import KFold
 from tqdm import tqdm
 
 from datasets import create_dataloaders
 from metrics import mAP
 from week_utils import (convertAnnotations, readXMLtoAnnotation,
-                        split_strategy_A, split_strategy_B, split_strategy_C,
-                        split_video)
+                        split_strategy_A,
+                        split_video, split_strategy_B, split_strategy_C)
 
 NUM_EPOCHS = 1
 LEARNING_RATE = 0.005
@@ -79,11 +80,10 @@ def train(model, train_loader, test_loader, device, annotations):
             optimizer.step()
 
         value_mAP = evaluate(model, test_loader, device, annotations)
-        if epoch % 10 == 0:
-            print(f"Epoch: {epoch}, mAP: {value_mAP}, loss: {losses}")
+        print(f"Epoch: {epoch}, mAP: {value_mAP}, loss: {losses}")
+    print("Training finished")
 
-
-def run_finetuning(device="cpu", strategy="A", fold_idx=0):
+def run_finetuning(device="cpu", strategy="A"):
     annotations = readXMLtoAnnotation(ANNOTATIONS_PATH)
     formatted_annotations = convertAnnotations(annotations)
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -94,36 +94,65 @@ def run_finetuning(device="cpu", strategy="A", fold_idx=0):
         ]
     )
     n_frames = len(frames)
+
     if strategy == "A":
         train_idxs, test_idxs = split_strategy_A(n_frames)
-    elif strategy == "B":  # Fold 0 is the same as Strategy A
-        train_idxs, test_idxs = split_strategy_B(fold_idx, n_frames)
+        train_loader, test_loader = create_dataloaders(
+            formatted_annotations,
+            VIDEO_PATH,
+            train_idxs,
+            test_idxs,
+            transformations,
+            batch_size=2,
+        )
+        model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True).to(
+            device
+        )
+
+        for param_idx, param in enumerate(model.parameters()):
+            if param_idx < len(list(model.parameters())) - UNFROZEN_LAYERS:
+                param.requires_grad = False
+
+        train(model, train_loader, test_loader, device, annotations)
+        map = evaluate(model, test_loader, device, annotations)
+        print(
+            f"Using NUM_EPOCHS = {NUM_EPOCHS} LEARNING_RATE = {LEARNING_RATE} MOMENTUM = {MOMENTUM} WEIGHT_DECAY = {WEIGHT_DECAY} UNFROZEN_LAYERS ={UNFROZEN_LAYERS} using strategy {strategy}"
+        )
+        print("Finetuned map: ", map)
+    elif strategy == "B" or strategy == "C":
+        maps = []
+        # K-Fold Cross-Validation
+        for fold_idx in range(4):
+            if strategy == "B":
+                train_idxs, test_idxs = split_strategy_B(fold_idx, n_frames)  # K-Fold split
+            else:
+                train_idxs, test_idxs = split_strategy_C(n_frames)  # Random split
+            print(f"Fold {fold_idx + 1}")
+            train_loader, test_loader = create_dataloaders(
+                formatted_annotations,
+                VIDEO_PATH,
+                train_idxs,
+                test_idxs,
+                transformations,
+                batch_size=2,
+            )
+            model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True).to(device)
+
+            for param_idx, param in enumerate(model.parameters()):
+                if param_idx < len(list(model.parameters())) - UNFROZEN_LAYERS:
+                    param.requires_grad = False
+
+            train(model, train_loader, test_loader, device, annotations)
+            map = evaluate(model, test_loader, device, annotations)
+            maps.append(map)
+            print(
+                f"Using NUM_EPOCHS = {NUM_EPOCHS} LEARNING_RATE = {LEARNING_RATE} MOMENTUM = {MOMENTUM} WEIGHT_DECAY = {WEIGHT_DECAY} UNFROZEN_LAYERS ={UNFROZEN_LAYERS} using strategy {strategy}"
+            )
+            print(f'Fold {fold_idx+1} map: {map}')
+        print(f"Average mAP (using strategy {strategy}): {sum(maps) / len(maps)}")
     else:
-        train_idxs, test_idxs = split_strategy_C(n_frames)
-
-    train_loader, test_loader = create_dataloaders(
-        formatted_annotations,
-        VIDEO_PATH,
-        train_idxs,
-        test_idxs,
-        transformations,
-        batch_size=2,
-    )
-    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True).to(
-        device
-    )
-
-    for param_idx, param in enumerate(model.parameters()):
-        if param_idx < len(list(model.parameters())) - UNFROZEN_LAYERS:
-            param.requires_grad = False
-
-    train(model, train_loader, test_loader, device, annotations)
-    map = evaluate(model, test_loader, device, annotations)
-    print(
-        f"Using NUM_EPOCHS = {NUM_EPOCHS} LEARNING_RATE = {LEARNING_RATE} MOMENTUM = {MOMENTUM} WEIGHT_DECAY = {WEIGHT_DECAY} UNFROZEN_LAYERS ={UNFROZEN_LAYERS} using strategy {strategy}"
-    )
-    print("Finetuned map: ", map)
+        raise ValueError("Invalid strategy provided. Please use 'A', 'B', or 'C'.")
 
 
 if __name__ == "__main__":
-    run_finetuning(device="cuda" if torch.cuda.is_available() else "cpu")
+    run_finetuning(device="cuda" if torch.cuda.is_available() else "cpu", strategy="A")
