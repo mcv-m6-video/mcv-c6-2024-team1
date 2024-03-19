5c04d4923b21a4094700a9d08ceb380614500ab7
Pau Vallespí <53973659+pauvallespi16@users.noreply.github.com>
2024-03-19 11:22:50 +0100

    Add thresholds for stopping detections

--- Week4/Task1/tracking_speed.py
+++ Week4/Task1/tracking_speed.py
@@ -8,20 +8,17 @@ from ViewTransformer import ViewTransformer
 from sort import *
 from week_utils import *
 import supervision as sv
-SAVE = False
-VISUALIZE = True
+from collections import defaultdict, deque
 
-RESULTS_PATH = "./results/"
-FILE_IN = "bbxs_clean.json"
-FILE_OUT = "kalman"
 
-AICITY_DATA_PATH = "AICity_data/train/S03/c010"
-VIDEO_OUT_PATH = "./results/kalman_tracking_vdo.avi"
+DEFAULT_RESULTS_PATH = "./results/"
+DEFAULT_FILE_IN = "bbxs_clean.json"
+DEFAULT_FILE_OUT = "kalman"
+DEFAULT_STOP_THRESHOLD = 10
+DEFAULT_SPEED_THRESHOLD = 3
 
 # Read gt file
-ANOTATIONS_PATH = "ai_challenge_s03_c010-full_annotation.xml"
 VIDEO_PATH = "../../Data/AICity_data_S03_C010/AICity_data/train/S03/c010/vdo.avi"
-GENERATE_VIDEO = True
 
 
 def get_centroid(box: np.ndarray):  # box [xmin,ymin,xmax,ymax]
@@ -30,12 +27,18 @@ def get_centroid(box: np.ndarray):  # box [xmin,ymin,xmax,ymax]
     return x_center, y_center
 
 
-from collections import defaultdict, deque
-
 video_info = sv.VideoInfo.from_video_path(VIDEO_PATH)
 
+
 class KalmanSpeed:
-    def __init__(self, max_age=30, iou_threshold=0.4, view_transformer: ViewTransformer = None):
+    def __init__(
+        self,
+        max_age=30,
+        iou_threshold=0.4,
+        view_transformer: ViewTransformer = None,
+        stop_threshold: int = DEFAULT_STOP_THRESHOLD,
+        speed_threshold: int = DEFAULT_SPEED_THRESHOLD,
+    ):
         self.max_age = int(max_age)
         self.iou_threshold = iou_threshold
         self.kalman_tracker = Sort(
@@ -45,6 +48,9 @@ class KalmanSpeed:
         self.n_frame = 0
         self.view_transformer = view_transformer
         self.coordinates = defaultdict(lambda: deque(maxlen=video_info.fps))
+        self.stop_threshold = stop_threshold
+        self.speed_threshold = speed_threshold
+
     def next_frame(self):
         self.n_frame += 1
 
@@ -66,6 +72,7 @@ class KalmanSpeed:
             boxes.append(np.array(box))
 
         return np.array(boxes)
+
     def update(self, detected_bbox):
         # Convert Detected BBoxes as arrays
         detected_bboxes = self.convert_to_array(detected_bbox)
@@ -98,21 +105,30 @@ class KalmanSpeed:
         }
         self.n_frame += 1
 
-    def calculate_speed(self, tracker_id):
+    def calculate_speed(self, tracker_id, count_speed):
         # Wait to have enough data
-        if len(self.coordinates[tracker_id]) > video_info.fps / 2: #  if we have at least 0.5 seconds of data
+        if (
+            len(self.coordinates[tracker_id]) > video_info.fps / 2
+        ):  #  if we have at least 0.5 seconds of data
             # Calculate the speed
             coordinate_start = self.coordinates[tracker_id][-1]  # tuple
             coordinate_end = self.coordinates[tracker_id][0]  # tuple
             # compute distance between the first and last coordinates (tuples)
-            distance = np.sqrt((coordinate_end[0] - coordinate_start[0]) ** 2 + (coordinate_end[1] - coordinate_start[1]) ** 2)
+            distance = np.sqrt(
+                (coordinate_end[0] - coordinate_start[0]) ** 2
+                + (coordinate_end[1] - coordinate_start[1]) ** 2
+            )
             time = len(self.coordinates[tracker_id]) / video_info.fps
             speed = distance / time * 3.6
+            if speed < self.speed_threshold:
+                count_speed[tracker_id] += 1
+
             return speed
         return None
 
-    def draw_tracking_result(self, frame_img):
+    def draw_tracking_result(self, frame_img, count_speed):
         img_draw = frame_img.copy()
+
         for track in self.kalman_tracker.trackers:
 
             # Don't show non-updated trackers
@@ -121,15 +137,20 @@ class KalmanSpeed:
 
             # Get centroid of the bounding box
             centroid = get_centroid(convert_x_to_bbox(track.kf.x))
-            inside = centroid[0] > 580 and centroid[0] < 1110 and centroid[1] > 200 and centroid[1] < 1050
-            old_centroid = centroid
+            inside = (
+                centroid[0] > 580
+                and centroid[0] < 1110
+                and centroid[1] > 200
+                and centroid[1] < 1050
+            )
 
             # Transform centroid using ViewTransformer
             if self.view_transformer:
                 # Check if centroid is within the source zone
                 if inside:
                     transformed_centroid = self.view_transformer.transform_points(
-                        np.array([[centroid[0], centroid[1]]]))
+                        np.array([[centroid[0], centroid[1]]])
+                    )
                     centroid = tuple(transformed_centroid.squeeze())
                     self.coordinates[track.id].append(centroid)
                 else:
@@ -140,6 +161,10 @@ class KalmanSpeed:
             self.coordinates[track.id].append(centroid)
 
             box = convert_x_to_bbox(track.kf.x).squeeze()
+            speed = self.calculate_speed(track.id, count_speed)
+            if count_speed[track.id] > self.stop_threshold:
+                continue
+
             # Draw Bounding Box
             img_draw = cv2.rectangle(
                 img_draw,
@@ -157,7 +182,7 @@ class KalmanSpeed:
                 -1,
             )
             # Write Text with speed if in source zone, otherwise only write ID
-            speed = self.calculate_speed(track.id)
+
             if speed is not None and inside:
                 text = f"{track.id} {speed:.2f} km/h" if speed > 0 else f"{track.id}"
                 img_draw = cv2.putText(
@@ -192,9 +217,9 @@ class KalmanSpeed:
     def execute(
         self,
         video_path: str = VIDEO_PATH,
-        results_path: str = RESULTS_PATH,
-        file_in: str = FILE_IN,
-        file_out: str = FILE_OUT,
+        results_path: str = DEFAULT_RESULTS_PATH,
+        file_in: str = DEFAULT_FILE_IN,
+        file_out: str = DEFAULT_FILE_OUT,
         generate_video: bool = True,
         vizualize: bool = True,
     ):
@@ -215,6 +240,7 @@ class KalmanSpeed:
             )
 
         num_frame = 0
+        count_speed = defaultdict(int)
         while True:
             ret, frame_img = cap.read()
             if not ret:
@@ -223,7 +249,7 @@ class KalmanSpeed:
             # Predict
             self.update(frame_boxes[num_frame])
             # Draw
-            img_draw = self.draw_tracking_result(frame_img)
+            img_draw = self.draw_tracking_result(frame_img, count_speed)
 
             if vizualize:
                 cv2.imshow(
@@ -256,18 +282,41 @@ if __name__ == "__main__":
     parser.add_argument(
         "--results_path",
         type=str,
-        default=RESULTS_PATH,
+        default=DEFAULT_RESULTS_PATH,
         help="Path to the results directory",
     )
     parser.add_argument(
-        "--detections", type=str, default=FILE_IN, help="Input json with detections"
+        "--detections",
+        type=str,
+        default=DEFAULT_FILE_IN,
+        help="Input json with detections",
     )
-    parser.add_argument("--o_name", type=str, default=FILE_OUT, help="Output file name")
     parser.add_argument(
-        "--store", type=bool, default=False, help="Flag to generate video output"
+        "--o_name", type=str, default=DEFAULT_FILE_OUT, help="Output file name"
     )
     parser.add_argument(
-        "--vizualize", type=bool, default=False, help="Flag to generate video output"
+        "--store",
+        action="store_true",
+        default=False,
+        help="Flag to generate video output",
+    )
+    parser.add_argument(
+        "--vizualize",
+        action="store_true",
+        default=False,
+        help="Flag to generate video output",
+    )
+    parser.add_argument(
+        "--stop-threshold",
+        type=int,
+        default=DEFAULT_STOP_THRESHOLD,
+        help="Threshold to stop the detection when it has been stop more than `stop-threshold` times",
+    )
+    parser.add_argument(
+        "--speed-threshold",
+        type=int,
+        default=DEFAULT_SPEED_THRESHOLD,
+        help="Threshold to stop detecting speed",
     )
 
     # Kalman Filter Vars
@@ -281,7 +330,13 @@ if __name__ == "__main__":
     source = np.array([[550, 150], [1100, 150], [100, 1050], [1900, 1050]])
     target = np.array([[0, 0], [15, 0], [0, 200], [15, 200]])
     vt = ViewTransformer(source, target)
-    kalmanSpeedTracking = KalmanSpeed(max_age=args.max_age, iou_threshold=args.thr, view_transformer=vt)
+    kalmanSpeedTracking = KalmanSpeed(
+        max_age=args.max_age,
+        iou_threshold=args.thr,
+        view_transformer=vt,
+        stop_threshold=args.stop_threshold,
+        speed_threshold=args.speed_threshold,
+    )
 
     kalmanSpeedTracking.execute(
         video_path=args.video_path,
@@ -292,5 +347,4 @@ if __name__ == "__main__":
         vizualize=args.vizualize,
     )
 
-#python3 tracking_speed.py --store True --vizualize False --o_name kalman_tracking_vdo --detections bbxs_clean_tracked.json
-
+# python3 tracking_speed.py --store --vizualize --o_name kalman_tracking_vdo --detections bbxs_clean_tracked.json
