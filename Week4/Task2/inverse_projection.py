61863a12a1311df9be3071f4243e1c71b1ccbfff
blastoise-pro <32550964+blastoise-pro@users.noreply.github.com>
2024-03-20 22:31:18 +0100

    positional tracking

--- Week4/Task2/inverse_projection.py
+++ Week4/Task2/inverse_projection.py
@@ -4,12 +4,18 @@ import pickle
 import numpy as np
 from numpy.typing import ArrayLike
 import cv2
-import matplotlib.cm as cm
+import matplotlib as mpl
+import re
 import torch
 
 from syn_cam import *
 from mot.tracklet import Tracklet
 
+
+DISTANCE_THRESHOLD = 5
+BREAK_DISTANCE = 0.001
+EARTH_RADIUS = 6371001
+DEG2RAD = np.pi / 180
 
 def read_corners(path):
     with open(path) as f:
@@ -31,7 +37,7 @@ class MultiCameraTracklet:
         self.tracks = tracks
 
 
-class PositionalMultiCameraTrack:
+class MultiCameraTrackScene:
     def __init__(self, tl_coords: ArrayLike | tuple[float, float], br_coords: ArrayLike | tuple[float, float], image_path: Path | str, offset: ArrayLike | tuple[float, float] = (0, 0)):
         self.tl_corner = np.array(tl_coords)
         self.br_corner = np.array(br_coords)
@@ -45,6 +51,7 @@ class PositionalMultiCameraTrack:
         print(self.bg_image.shape)
 
         self.coords_to_pixels_factor = np.array([-self.bg_image.shape[0] / delta_theta, self.bg_image.shape[1] / delta_phi])
+        self.coords_to_meters_factor = np.array([DEG2RAD * EARTH_RADIUS, DEG2RAD * EARTH_RADIUS * np.cos(tl_coords[0])])
 
         self.cameras: dict[int, np.ndarray] = {}
         self.objects: dict[int, np.ndarray] = {}
@@ -53,10 +60,14 @@ class PositionalMultiCameraTrack:
         if id in self.cameras:
             raise ValueError(f"Camera with id {id} already exists.")
         self.cameras[id] = np.linalg.inv(calibration)
+
+        for i, point in enumerate(itertools.product([0, 1920], np.linspace(200, 1080, num=40))):
+            visualization.add_object(f"cam{id}b_{i}", id, point)
     
 
-
     def add_object(self, id: int, camera_id: int, pixel_position: ArrayLike | tuple[float, float]):
+        # Pixel position in (x, y) format from top left corner
+
         if id in self.objects:
             raise ValueError(f"Object with id {id} already exists.")
         #print("Adding object")
@@ -66,10 +77,17 @@ class PositionalMultiCameraTrack:
         #print(world_homo)
         self.objects[id] = world_homo[:2] / world_homo[2]
         #print(self.objects[id])
-    
+        return self.objects[id]
 
-    
-    def plot(self, show_image = True):
+    def reset(self):
+        for obj in self.objects:
+            if "cam" not in obj:
+                self.objects.pop(obj)
+
+
+    def plot(self, show_image = True, show_fovs: list[int | str] = None):
+        if show_fovs is None:
+            show_fovs = self.cameras.keys()
         print("Plotting:")
         if show_image:
             image = self.bg_image.copy()
@@ -81,6 +99,10 @@ class PositionalMultiCameraTrack:
             #print((gps_coords - self.tl_corner))
             map_pos = (gps_coords - self.tl_corner) * self.coords_to_pixels_factor
             #print(map_pos)
+            if type(id) == str:
+                num = re.match(r"\d+", id).group()
+                if num not in show_fovs:
+                    continue
             if id[:5] == "cam0b":
                 color = (255, 0, 0)
             if id[:5] == "cam1b":
@@ -93,11 +115,75 @@ class PositionalMultiCameraTrack:
                 color = (255, 0, 255)
             if id[:5] == "cam5b":
                 color = (255, 255, 0)
+            if type(id) == int:
+                color = np.round(mpl.colormaps["tab20"](id) * 255).astype(np.uint8)[::-1]
             cv2.circle(image, np.int32([map_pos[0], map_pos[1]]), radius=5, color=color, thickness=-1)
         cv2.imshow("ZenithalView", image)
-        cv2.waitKey(10)
+        cv2.waitKey(1)
 
+
+def get_bbox_center(bbox):
+    return (bbox[1] + bbox[2]/2, bbox[0] + bbox[3]/2) # Return (x, y) from top left corner
+
+
+def positional_match(track1: Tracklet, track2: Tracklet, scene: MultiCameraTrackScene):
+    frame_intersect_start = max(track1.global_start, track2.global_start)
+    frame_intersect_end = min(track1.global_end, track2.global_end)
+    time_intersecting = True if frame_intersect_start < frame_intersect_end else False
+
+    if time_intersecting:
+        frame_matches = 0
+        for frame in range(frame_intersect_start, frame_intersect_end + 1):
+            track1_cam_frame = (frame - track1.global_start)
+            track2_cam_frame = (frame - track2.global_start)
+            if track1_cam_frame in track1.frames and track2_cam_frame in track2.frames:
+                bbox1_center = get_bbox_center(track1.bboxes[track1.frames.index(track1_cam_frame)])
+                bbox2_center = get_bbox_center(track2.bboxes[track2.frames.index(track2_cam_frame)])
+            else:
+                continue
+            pos1 = scene.add_object(track1.track_id, track1.cam, bbox1_center)
+            pos2 = scene.add_object(track2.track_id, track2.cam, bbox2_center)
+            distance = np.sqrt(np.sum(((pos1 - pos2) * scene.coords_to_meters_factor)**2))
+
+            if distance < DISTANCE_THRESHOLD:
+                frame_matches += 1
+            
+            if distance > BREAK_DISTANCE:
+                print("Too far away, breaking")
+                scene.plot(show_image=False, show_fovs=[track1.cam, track2.cam])
+                scene.reset()
+                return False
+            
+            scene.plot(show_image=False, show_fovs=[track1.cam, track2.cam])
+            scene.reset()
         
+        if frame_matches / (frame_intersect_end - frame_intersect_start) > 0.8:
+            return True
+        else:
+            return False
+    else:
+        first_track: Tracklet = min(track1, track2, key=lambda t: t.global_end)
+        second_track: Tracklet = max(track1, track2, key=lambda t: t.global_end)
+        #TODO: Use speed estimation part?
+        last_bbx_centers = map(get_bbox_center, first_track.bboxes[-5:])
+        last_positions = np.array(map(lambda x: scene.add_object(x, first_track.cam, x), last_bbx_centers))
+        last_velocities = [last_positions[i] - last_positions[i-1] for i in range(1, len(last_positions))]
+        estimated_velocity = np.mean(last_velocities, axis=0)
+
+        frame_diff = frame_intersect_start - frame_intersect_end
+        estimated_position = estimated_velocity * frame_diff + last_positions[-1]
+
+        track2_start_position = scene.add_object(1, second_track.cam, get_bbox_center(second_track.bboxes[0]))
+        distance = np.sqrt(np.sum(((estimated_position - track2_start_position) * scene.coords_to_meters_factor)**2))
+
+        scene.reset()
+        return distance < DISTANCE_THRESHOLD
+
+
+
+def merge_tracks(track1: Tracklet, track2: Tracklet):
+    pass
+
 
 if __name__ == "__main__":
     sequence_name = "S03"
@@ -107,20 +193,28 @@ if __name__ == "__main__":
     # Write top left and bottom right GPS coordinates of the image in 2 lines
     corners_path = f"./Week4/VisualizationData/{sequence_name}/corners.txt"
     tl_corner, br_corner = read_corners(corners_path)
-    visualization = PositionalMultiCameraTrack(tl_corner, br_corner, bg_image_path, offset=(0.0002, 0.0005))
+    visualization = MultiCameraTrackScene(tl_corner, br_corner, bg_image_path, offset=(0.0002, 0.0005), show_fov=True)
     
     for i, name in enumerate(camera_names):
         calibration_path = f"./Data/aic19-track1-mtmc-train/train/{sequence_name}/{name}/calibration.txt"
         visualization.add_camera(i, read_calibration(calibration_path))
 
-    for i, point in enumerate(itertools.product([0, 1920], np.linspace(200, 1920, num=40))):
+    for i, point in enumerate(itertools.product([0, 1920], np.linspace(200, 1080, num=40))):
         for j in range(len(camera_names)):
             visualization.add_object(f"cam{j}b_{i}", j, point)
             #visualization.plot(False)
     #visualization.plot(False)
-    #cv2.waitKey(0)
+    cv2.waitKey(0)
     
-    all_tracks, t_compa , global_frames = syncronize_trackers()
+    all_tracks, t_compa , global_frames = syncronize_trackers(camera_names)
+
+    # Check position coincidences
+    for (i, j) in set(map(frozenset, np.argwhere(t_compa == 1))):
+        track1: Tracklet = all_tracks[i]
+        track2: Tracklet = all_tracks[j]
+        
+        if positional_match(track1, track2):
+            merge_tracks(track1, track2)
 
      # precompute similarities between tracks
     f = torch.Tensor(np.stack([tr.mean_feature for tr in all_tracks]))
